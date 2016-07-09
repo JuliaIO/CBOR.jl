faa0c33ea5b9fde2ba16e2128e474966cee26123
Saurav Sachidanand <sauravsachidanand@gmail.com>
2016-07-09 19:59:41 +0530

    Add boolean encoding

--- src/CBOR.jl
+++ src/CBOR.jl
@@ -1,9 +1,19 @@
 module CBOR
 
+function encode(bool::Bool)
+    if bool
+        return UInt8[0xf5,]
+    end
+    return UInt8[0xf4,]
+end
+
 function encode(data)
+    cbor_bytes = UInt8[]
+    return cbor_bytes
 end
 
 function decode(code)
+
 end
 
 end
