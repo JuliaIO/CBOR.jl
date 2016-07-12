9c7bd7966f9fea0fe432fb3b350d67f117d700a9
Saurav Sachidanand <sauravsachidanand@gmail.com>
2016-07-12 16:42:11 +0530

    Add ascii and utf8 string decoding

--- src/CBOR.jl
+++ src/CBOR.jl
@@ -154,6 +154,24 @@ function decode_next(start_idx, bytes::Array{UInt8, 1})
             data, bytes_consumed = decode_unsigned(start_idx, bytes)
             data = -(Signed(data) + 1)
             data, bytes_consumed
+        elseif typ == TYPE_2
+            string_len, bytes_consumed = decode_unsigned(start_idx, bytes)
+
+            start_idx += bytes_consumed
+            string =
+                ASCIIString(bytes[start_idx:(start_idx + string_len - 1)])
+            bytes_consumed += string_len
+
+            string, bytes_consumed
+        elseif typ == TYPE_3
+            string_bytes, bytes_consumed = decode_unsigned(start_idx, bytes)
+
+            start_idx += bytes_consumed
+            string =
+                UTF8String(bytes[start_idx:(start_idx + string_bytes - 1)])
+            bytes_consumed += string_bytes
+
+            string, bytes_consumed
         elseif typ == TYPE_4
             vec_len, bytes_consumed = decode_unsigned(start_idx, bytes)
             data = Vector(vec_len)
